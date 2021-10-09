acf6f8cf65d332d38982a65d806ca7b8b6a1f3d7
agamstawn <agam.setiawan@nutrifood.co.id>
2021-10-09 10:22:33 +0700

    Dry Code

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -16,31 +16,15 @@ class ReservationsController < ApplicationController
     phones_arr = []
     payloads.each do |input|
       if input.last.class.name == 'String'
-        # reservation.code = input.last if input.first.include?('code')
-        # reservation.currency = input.last if input.first.include?('currency')
-        # reservation.end_date = input.last.to_datetime if input.first.include?('end_date')
-        # reservation.payout_price = input.last.to_f if input.first.include?('payout')
-        # reservation.security_price = input.last.to_f if input.first.include?('security_price')
-        # reservation.start_date = input.last.to_datetime if input.first.include?('start_date')
-        # reservation.status = input.last if input.first.include?('status')
-        # reservation.total_price = input.last.to_f if input.first.include?('total')
         update_reservation(reservation, input)
       elsif input.last.class.name == 'Integer'
-        # guest.adults = input.last if input.first.include?('adults')
-        # guest.children = input.last if input.first.include?('children')
-        # guest.infants = input.last if input.first.include?('infants')
         update_guest(guest, input)
-        # reservation.nights = input.last if input.first.include?('nights')
-        # reservation.guests = input.last if input.first.include?('guests')
         update_reservation(reservation, input)
       else
         if input.first.include?('guest')
           guest_dtls = input.last.to_enum.to_h
           guest_dtls.each do |gd|
             update_guest(guest, gd)
-            # guest.email = gd.last if gd.first.include?('email')
-            # guest.first_name = gd.last if gd.first.include?('first_name')
-            # guest.last_name = gd.last if gd.first.include?('last_name')
             if gd.first.include?('phone')
               phone.guest_id = guest.id
               phone.phone_number = gd.last
@@ -50,26 +34,10 @@ class ReservationsController < ApplicationController
           res_dtls = input.last.to_enum.to_h
           res_dtls.each do |rd|
             update_guest(guest, rd)
-            # guest.email = rd.last if rd.first.include?('email')
-            # guest.first_name = rd.last if rd.first.include?('first_name')
-            # guest.last_name = rd.last if rd.first.include?('last_name')
-            # reservation.code = rd.last if rd.first.include?('code')
-            # reservation.currency = rd.last if rd.first.include?('currency')
-            # reservation.end_date = rd.last.to_datetime if rd.first.include?('end_date')
-            # reservation.payout_price = rd.last.to_f if rd.first.include?('payout')
-            # reservation.security_price = rd.last.to_f if rd.first.include?('security_price')
-            # reservation.start_date = rd.last.to_datetime if rd.first.include?('start_date')
-            # reservation.status = rd.last if rd.first.include?('status')
-            # reservation.total_price = rd.last.to_f if rd.first.include?('total')
-            # reservation.nights = rd.last if rd.first.include?('nights')
-            # reservation.guests = rd.last if rd.first.include?('guests')
             update_reservation(reservation, rd)
             if rd.first.include?('guest_details')
               rd_guest_dtls = rd.last.to_enum.to_h
               rd_guest_dtls.each do |rd_gd|
-                # guest.adults = rd_gd.last if rd_gd.first.include?('adults')
-                # guest.children = rd_gd.last if rd_gd.first.include?('children')
-                # guest.infants = rd_gd.last if rd_gd.first.include?('infants')
                 update_guest(guest, rd_gd)
               end
             elsif rd.first.include?('phone')
